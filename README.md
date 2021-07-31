03c65fd8851d35e02bfb379b69a9e443360b679e
rzbrk <email@jan-grosser.de>
2021-07-31 18:16:35 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Diese Seite bei [https://rzbrk.github.io/30s-iss-sensors/](https://rzbrk.github.io/30s-iss-sensors/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/rzbrk/30s-iss-sensors** suchen und importieren
+
+## Dieses Projekt bearbeiten ![Build Status Abzeichen](https://github.com/rzbrk/30s-iss-sensors/workflows/MakeCode/badge.svg)
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/rzbrk/30s-iss-sensors** ein und klicke auf Importieren
+
+## Blockvorschau
+
+Dieses Bild zeigt den Blockcode vom letzten Commit im Master an.
+Die Aktualisierung dieses Bildes kann einige Minuten dauern.
+
+![Eine gerenderte Ansicht der Blöcke](https://github.com/rzbrk/30s-iss-sensors/raw/master/.github/makecode/blocks.png)
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,71 @@
+function checkTimeout () {
+    if (control.millis() >= startTime + runMaxSeconds * 1000) {
+        runProgram = false
+        basic.showIcon(IconNames.Yes)
+        serial.writeLine("@END@")
+    }
+}
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    SERIAL_RECEIVED = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    if ("@START@" == SERIAL_RECEIVED.substr(0, 7) && !(runProgram)) {
+        serial.writeLine("@START@")
+        basic.clearScreen()
+        runProgram = true
+        startTime = control.millis()
+    }
+})
+let tick = 0
+let SERIAL_RECEIVED = ""
+let startTime = 0
+let runProgram = false
+let runMaxSeconds = 0
+// runMaxSeconds is the maximum time in seconds the program is allowed to run.
+runMaxSeconds = 30
+runProgram = false
+let counter = 0
+let testAccelerometer = true
+let testMagnetometer = false
+let testSCD30 = true
+let testSI1145 = false
+let testTCS34725 = false
+basic.showIcon(IconNames.Asleep)
+basic.forever(function () {
+    if (runProgram) {
+        tick = control.millis()
+        serial.writeLine("Loop #" + counter)
+        serial.writeLine("  Testing Calliope on-board sensors . . .")
+        serial.writeLine("    Temp: " + input.temperature() + " degrees")
+        serial.writeLine("    Light Intensity: " + input.lightLevel())
+        if (testAccelerometer == true) {
+            serial.writeLine("    Acc x: " + input.acceleration(Dimension.X) + " mg")
+            serial.writeLine("    Acc y: " + input.acceleration(Dimension.Y) + " mg")
+            serial.writeLine("    Acc z: " + input.acceleration(Dimension.Z) + " mg")
+            serial.writeLine("    Acc total: " + input.acceleration(Dimension.Strength) + " mg")
+        }
+        if (testMagnetometer == true) {
+            serial.writeLine("    Mag x: " + input.magneticForce(Dimension.X) + " µT")
+            serial.writeLine("    Mag y: " + input.magneticForce(Dimension.Y) + " µT")
+            serial.writeLine("    Mag z: " + input.magneticForce(Dimension.Z) + " µT")
+            serial.writeLine("    Mag total: " + input.magneticForce(Dimension.Strength) + " µT")
+        }
+        if (testSCD30 == true) {
+            serial.writeLine("  Testing SCD30 . . .")
+            serial.writeLine("    Sensor version: " + SCD30.getVersion())
+            serial.writeLine("    Temp: " + SCD30.readTemperature() + " degrees")
+            serial.writeLine("    Humidity: " + SCD30.readHumidity() + " %")
+            serial.writeLine("    CO2: " + SCD30.readCO2() + " ppm")
+        }
+        if (testSI1145 == true) {
+            serial.writeLine("  Testing SI1145 . . .")
+        }
+        if (testTCS34725 == true) {
+            serial.writeLine("  Testing TCS34725 . . .")
+        }
+        while (control.millis() - tick < 1000) {
+        	
+        }
+        counter = counter + 1
+        serial.writeLine("")
+        checkTimeout()
+    }
+})
